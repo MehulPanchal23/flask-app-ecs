27df00d16b3fa66c5f8a87469912a3bbdd13b735
Mehul Panchal <panchalmehul195@gmail.com>
2025-03-31 16:34:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Mehul, welcome to DevOps Zero To Hero (Junoon  Batch 9) Lets Rock People'
-    return 'Helloooo Doston Ye Line mene update ki hai check karne ke liye'
+    return 'Hello Mehul, welcome to DevOps Zero To Hero (Junoon  Batch 9) Lets Rockssss Peoplesssssssss'
 
 @app.route('/health')
 def health():
